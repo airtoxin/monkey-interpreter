2e520a9866595a2e04c3cb7f7ec275b4c0893a81
airtoxin <airtoxin@icloud.com>
2019-11-27 11:33:01 +0900

    Add call expression

--- pkg/ast/expression.go
+++ pkg/ast/expression.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"monkey-interpreter/pkg/token"
+	"strings"
 )
 
 type Expression interface {
@@ -81,5 +82,32 @@ func (ie *IfExpression) String() string {
 		out.WriteString("else ")
 		out.WriteString(ie.Alternative.String())
 	}
+	return out.String()
+}
+
+type CallExpression struct {
+	Token     token.Token // (
+	Function  Expression  // Identifier or FunctionLiteral
+	Arguments []Expression
+}
+
+func (ce *CallExpression) expressionNode() {}
+
+func (ce *CallExpression) TokenLiteral() string {
+	return ce.Token.Literal
+}
+
+func (ce *CallExpression) String() string {
+	var out bytes.Buffer
+
+	args := []string{}
+	for _, a := range ce.Arguments {
+		args = append(args, a.String())
+	}
+
+	out.WriteString(ce.Function.String())
+	out.WriteString("(")
+	out.WriteString(strings.Join(args, ", "))
+	out.WriteString(")")
 	return out.String()
 }
--- pkg/ast/literal.go
+++ pkg/ast/literal.go
@@ -1,6 +1,10 @@
 package ast
 
-import "monkey-interpreter/pkg/token"
+import (
+	"bytes"
+	"monkey-interpreter/pkg/token"
+	"strings"
+)
 
 type IntegerLiteral struct {
 	Token token.Token
@@ -15,4 +19,30 @@ func (il *IntegerLiteral) TokenLiteral() string {
 
 func (il *IntegerLiteral) String() string {
 	return il.TokenLiteral()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token // token.FUNCTION
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {}
+
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+	var params []string
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+	return out.String()
 }
